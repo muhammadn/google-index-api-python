6d42f5d04b583f6a869c3b6cadadde9c549e8b05
Muhammad Nuzaihan <muhammad@taqisystems.com>
2020-11-20 04:22:10 +0800

    fix more bugs in the code

--- submit_urls_batch.py
+++ submit_urls_batch.py
@@ -47,33 +47,38 @@ def callback(request_id, response, exception):
     # Do something with the exception
     #   pass
         print('An exception occurred: ', exception)
-    else:
-    # Do something with the response
     #   pass
         print('request_id is: ', request_id)
         print('response is: ', response)
 
-batch = service.new_batch_http_request(callback=callback)
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
 def process(content):
-    postBody = service.urlNotifications().publish(body = content)
-    batch.add(postBody)
-    batch.execute()
+    urls = list(chunks(content, 1000))
+    for x in urls:
+        print('Submitting URL to Google Index API in batches of 10000')
+        print('Now running ' + str(len(x)) + ' URLs')
+        batch = service.new_batch_http_request(callback=callback)
+        urldata = []
+        for y in x:
+            postBody = service.urlNotifications().publish(body = y)
+            batch.add(postBody)
+        batch.execute()
+        print('Done!')
 
 with open('city.csv', newline='') as csvfile:
     city = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in city:
         city = row[0].replace(' ', '-').lower()
         for x in languages:
-            url = 'https://www.tutoroo.co/' + x + '-tutor-' + city
-            #print(url)
-            urls.append(BASE_URL + x + '-tutor-' + city)
+            url = BASE_URL + x + '-tutor-' + city
+            urls.append(url)
             content = {"url": url, "type": "URL_UPDATED"}
             content_array.append(content)
 
-for i in range(0, len(content_array), 1000):
-    print('Sending to Google Index API...')
-    process(content_array[i:i+1000])
-    print('Done')
+process(content_array)
 
-print("number of URLs: " + str(len(urls)))
+print("Total number of URLs: " + str(len(urls)))
